eecf50700059c5e6e8d607b096c5871ef12dee7a
Faiz Authar <faizauthar@gmail.com>
2022-04-20 12:31:41 +0700

    Permission: Only need camera

--- app/src/main/java/com/android/gunadarma/daunteh/ui/main/MainActivity.kt
+++ app/src/main/java/com/android/gunadarma/daunteh/ui/main/MainActivity.kt
@@ -86,14 +86,6 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "DaunTeh"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf(
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
