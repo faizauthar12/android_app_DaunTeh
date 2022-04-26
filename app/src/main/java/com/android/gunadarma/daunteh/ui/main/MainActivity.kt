2f166702436fb51a23eb2e6758738a359346ebb3
Faiz Authar <faizauthar@gmail.com>
2022-04-26 11:58:18 +0700

    App: Implement tensorflow image analyzer

--- app/src/main/java/com/android/gunadarma/daunteh/ui/main/MainActivity.kt
+++ app/src/main/java/com/android/gunadarma/daunteh/ui/main/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.android.gunadarma.daunteh.ui.main
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.android.gunadarma.daunteh.BuildConfig
 import com.android.gunadarma.daunteh.databinding.ActivityMainBinding
 import com.android.gunadarma.daunteh.databinding.ContentMainRecognitionResultBinding
-import java.lang.Exception
+import com.android.gunadarma.daunteh.ml.FinalModel
+import com.android.gunadarma.daunteh.util.YuvToRgbConverter
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.common.TensorProcessor
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.model.Model
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -39,8 +56,6 @@ class MainActivity : AppCompatActivity() {
 
         // Executor Service
         cameraExecutor = Executors.newSingleThreadExecutor()
-
-        contentMainBinding.tvCondition.text = "Kondisi: Layak"
     }
 
     private fun startCamera() {
@@ -55,6 +70,23 @@ class MainActivity : AppCompatActivity() {
                 .build()
                 .also { it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider) }
 
+            val imageAnalyzer = ImageAnalysis.Builder()
+                // This sets the ideal size for the image to be analyse, CameraX will choose the
+                // the most suitable resolution which may not be exactly the same or hold the same
+                // aspect ratio
+                .setTargetResolution(Size(224, 224))
+                // How the Image Analyser should pipe in input, 1. every frame but drop no frame, or
+                // 2. go to the latest frame and may drop some frame. The default is 2.
+                // STRATEGY_KEEP_ONLY_LATEST. The following line is optional, kept here for clarity
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also { analysisUseCase: ImageAnalysis ->
+                    analysisUseCase.setAnalyzer(
+                        cameraExecutor,
+                        ImageAnalyzer(this, contentMainBinding)
+                    )
+                }
+
             // Select back camera as default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -63,12 +95,107 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.unbindAll()
 
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
             } catch (e: Exception) {
                 // Catch exception
                 Log.e(TAG, "Use case binding failed", e)
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    class ImageAnalyzer(
+        ctx: Context,
+        private val binding: ContentMainRecognitionResultBinding /*private val listener: RecognitionListener*/
+    ) :
+        ImageAnalysis.Analyzer {
+
+        private val finalModel: FinalModel by lazy {
+            // TODO 6. Optional GPU acceleration
+            val compatList = CompatibilityList()
+
+            val options = if (compatList.isDelegateSupportedOnThisDevice) {
+                Log.d(TAG, "This device is GPU Compatible ")
+                Model.Options.Builder().setDevice(Model.Device.GPU).build()
+            } else {
+                Log.d(TAG, "This device is GPU Incompatible ")
+                Model.Options.Builder().setNumThreads(4).build()
+            }
+
+            FinalModel.newInstance(ctx, options)
+        }
+
+        override fun analyze(imageProxy: ImageProxy) {
+            var tfImage = TensorImage(DataType.FLOAT32)
+            tfImage.load(toBitmap(imageProxy))
+
+            val imageProcessor = ImageProcessor.Builder()
+                .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+                .build()
+
+            tfImage = imageProcessor.process(tfImage)
+
+            val probabilityProcessor = TensorProcessor.Builder().add(NormalizeOp(0f, 255f)).build()
+
+            val outputs = finalModel.process(probabilityProcessor.process(tfImage.tensorBuffer))
+
+            val outputBuffer = outputs.outputFeature0AsTensorBuffer
+
+            val tensorLabel =
+                TensorLabel(arrayListOf("layak_olah", "tidak_layak_olah"), outputBuffer)
+
+            val probability = tensorLabel.mapWithFloatValue["layak_olah"]
+            probability?.let {
+                if (it > 0.40) {
+                    binding.tvCondition.text = "Kondisi: Layak Olah"
+                } else {
+                    binding.tvCondition.text = "Kondisi: Tidak Layak Olah"
+                }
+            }
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "Daun Teh : " + probability)
+            }
+
+            imageProxy.close()
+        }
+
+        /**
+         * Convert Image Proxy to Bitmap
+         */
+        private val yuvToRgbConverter = YuvToRgbConverter(ctx)
+        private lateinit var bitmapBuffer: Bitmap
+        private lateinit var rotationMatrix: Matrix
+
+        @SuppressLint("UnsafeExperimentalUsageError", "UnsafeOptInUsageError")
+        private fun toBitmap(imageProxy: ImageProxy): Bitmap? {
+
+            val image = imageProxy.image ?: return null
+
+            // Initialise Buffer
+            if (!::bitmapBuffer.isInitialized) {
+                // The image rotation and RGB image buffer are initialized only once
+                Log.d(TAG, "Initalise toBitmap()")
+                rotationMatrix = Matrix()
+                rotationMatrix.postRotate(imageProxy.imageInfo.rotationDegrees.toFloat())
+                bitmapBuffer = Bitmap.createBitmap(
+                    imageProxy.width, imageProxy.height, Bitmap.Config.ARGB_8888
+                )
+            }
+
+            // Pass image to an image analyser
+            yuvToRgbConverter.yuvToRgb(image, bitmapBuffer)
+
+            // Create the Bitmap in the correct orientation
+            return Bitmap.createBitmap(
+                bitmapBuffer,
+                0,
+                0,
+                bitmapBuffer.width,
+                bitmapBuffer.height,
+                rotationMatrix,
+                false
+            )
+        }
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
